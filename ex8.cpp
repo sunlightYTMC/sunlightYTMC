5ea8c81ad50eaa0b90311b5f0ecbe9eb4f14c3a4
sunlightYTMC <109333106+sunlightYTMC@users.noreply.github.com>
2022-07-15 16:52:07 +0800

    Create ex8.cpp

---
+++ ex8.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float a,b;
+    printf("please input two number \n ");
+    printf(" first number :\n ");
+    scanf("%f",&a);
+    printf(" second number :\n ");
+    scanf("%f",&b);
+    //printf(" my height is%2dm%3dcm \n",a,b);
+    printf("%f*%f +%f*%f = %f \n",a,a,b,b,a*a+b*b);
+    cout <<  "hello johnson";
+    //cout <<  "my height is " << a << "m " << b << "cm";
+
+    return 0;
+}
