8f421299bce96df242c42bb2e25c7b8a53a8b1fb
sunlightYTMC <109333106+sunlightYTMC@users.noreply.github.com>
2022-07-15 15:52:35 +0800

    Create ex5.cpp

---
+++ ex5.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a,b;
+    printf("please input two number \n ");
+    printf(" first number :\n ");
+    scanf("%d",&a);
+    printf(" second number :\n ");
+    scanf("%d",&b);
+    //printf(" my height is%2dm%3dcm \n",a,b);
+    printf("%d + %d = %d\n",a,b,a+b);
+    cout <<  "hello selena ";
+    //cout <<  "my height is " << a << "m " << b << "cm";
+
+    return 0;
+}
