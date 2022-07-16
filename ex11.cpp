c50b07bc09315a9bf375341b8d1663e901c0102c
sunlightYTMC <109333106+sunlightYTMC@users.noreply.github.com>
2022-07-16 11:34:18 +0800

    Create ex11.cpp

---
+++ ex11.cpp
@@ -0,0 +1,20 @@
+#include <iostream> 
+#include <stdio.h> 
+using namespace std;  
+int main() {
+    printf(" hello teacher ");
+    float a,b,c, product;
+    printf("please input a number\n"); 
+    scanf("%f",&a);
+    printf("please input a number\n"); 
+    scanf("%f",&b);
+    printf("please input a number\n"); 
+    scanf("%f",&c);
+    // Calculating product 
+    product = (a + b)* c/2 ;
+    printf("Product = %f \n",  product);
+    //printf("Product = %f \n", 2* 3.14159 a);
+    product =(a+b)*2 ;
+    printf("Product = %f \n",  product);
+    return 0; 
+}
