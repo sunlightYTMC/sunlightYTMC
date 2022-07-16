edc16a1a29d21bd6e1a0aa6814dc5bcb82d2a99b
sunlightYTMC <109333106+sunlightYTMC@users.noreply.github.com>
2022-07-16 14:34:56 +0800

    Create ex13.cpp

---
+++ ex13.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+ int main(void) { 
+        int x,y; 
+        printf("please input two number \n ");
+        printf(" first number :\n ");
+        scanf("%d",&x);
+        printf(" second number :\n ");
+        scanf("%d",&y);
+        x = 3; y= 4;
+                
+        if ( x > y ){ 
+            printf(" x > y \n"); 
+        }else{ 
+            printf(" x <= y \n");                 
+        
+        }
+         
+}
