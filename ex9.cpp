287c0661ff91559c5af8f42f101c4aeb375f8817
sunlightYTMC <109333106+sunlightYTMC@users.noreply.github.com>
2022-07-16 11:06:43 +0800

    Create ex9.cpp

---
+++ ex9.cpp
@@ -0,0 +1,18 @@
+#include <iostream> 
+#include <stdio.h> 
+using namespace std;  
+int main() {
+    printf(" hello teacher ");
+    float a, b, product; 
+    a = 20; 
+    b = 7; 
+    printf("please input a number\n"); 
+    scanf("%f",&a);
+    // Calculating product 
+    product = 3.14159*a * a;
+    printf("Product = %f \n",  product);
+    //printf("Product = %f \n", 2* 3.14159 a);
+    product =3.14159*2 * a;
+    printf("Product = %f \n",  product);
+    return 0; 
+}
