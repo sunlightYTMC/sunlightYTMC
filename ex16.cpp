ce4e7b84877414bf4b2a557cb8240bb66c1b260d
sunlightYTMC <109333106+sunlightYTMC@users.noreply.github.com>
2022-07-16 15:55:00 +0800

    Create ex16.cpp

---
+++ ex16.cpp
@@ -0,0 +1,22 @@
+#include <iostream> 
+#include <stdio.h> 
+using namespace std;  
+int main() {
+    printf(" hello teacher ");
+    float a, b, product; 
+    printf("please input a number\n");
+    scanf("%f",&a);
+    if(a >= 0){
+          // Calculating product 
+    product = 3.14159*a*a;
+    printf("Product = %f \n",  product);
+    //printf("Product = %f \n", 2* 3.14159* a);
+    product =3.14159*2 * a ;
+printf("Product = %f \n",  product);
+          printf(" r >= 0 \n");  
+    }else{
+          printf(" r <= 0 \n");
+         }     
+        
+    return 0; 
+}
