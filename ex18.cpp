ce9095408f0e7f5bc6235e8f9ab7e3695624bd88
sunlightYTMC <109333106+sunlightYTMC@users.noreply.github.com>
2022-07-17 10:27:50 +0800

    Create ex18.cpp

---
+++ ex18.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+ int main(void) { 
+        int a,b,sum; 
+        printf("please input two number \n ");
+        printf(" first number :\n ");
+        scanf("%d",&a);
+        printf(" second number :\n ");
+        scanf("%d",&b);
+        printf("please input the sum of two number \n ");
+        scanf("%d",& sum);
+        
+        if ( (a + b)==sum) { 
+            printf(" right  \n"); 
+        }else{ 
+            printf(" error \n");                 
+
+}
+         
+ }
