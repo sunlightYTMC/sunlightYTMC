01e8527f94ab1b02f2a49acf721ac737e07cd061
sunlightYTMC <109333106+sunlightYTMC@users.noreply.github.com>
2022-07-17 11:52:36 +0800

    Create ex20.cpp

---
+++ ex20.cpp
@@ -0,0 +1,16 @@
+#include <iostream> 
+using namespace std;
+int main() 
+{ 
+        int i,j; 
+
+        for(i=1;i<4;i++){
+            for(j=1;j<i+1;j++){
+                printf(" * ");
+           }printf("\n");
+           
+}           
+    
+        cout<<"Hello";
+        
+}
