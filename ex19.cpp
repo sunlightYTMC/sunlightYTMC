825860565d438df5a4a7baecb907911bbda2e56b
sunlightYTMC <109333106+sunlightYTMC@users.noreply.github.com>
2022-07-17 11:27:06 +0800

    Create ex19.cpp

---
+++ ex19.cpp
@@ -0,0 +1,16 @@
+#include <iostream> 
+using namespace std;
+int main() 
+{ 
+        int i,j; 
+
+        for(i=3;i<6;i++){
+            for(j=3;j<6;j++){
+                printf(" * ");
+           }
+           printf("\n");
+}           
+    
+        cout<<"Hello";
+        
+}
