97c6ccb15117461847dfb71360cfbdd5141d1cf4
sunlightYTMC <109333106+sunlightYTMC@users.noreply.github.com>
2022-07-24 10:29:48 +0800

    Create exx22.cpp

---
+++ exx22.cpp
@@ -0,0 +1,15 @@
+#include <iostream> 
+using namespace std;
+int main() 
+{ 
+        int i,j; 
+
+        for(i=1;i<8;i++){
+            for(j=1;j<i+1;j++){
+             printf("%d",i);
+             
+           }printf("\n");
+}           
+   
+                
+}
